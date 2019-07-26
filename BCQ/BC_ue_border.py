e738bd9e1907ce82997d57397824830f5de61137
lanyavik <48976716+lanyavik@users.noreply.github.com>
2019-07-26 16:05:06 +0800

    Update BC_ue_border.py
    the UpperEnvelope BC code using G(s)>b*V(s)

--- BCQ/BC_ue_border.py
+++ BCQ/BC_ue_border.py
@@ -83,7 +83,6 @@ class BC_ue(object):
 
 			logger.store(Loss=actor_loss.cpu().item(), UpSize=update_size, SVal=state_value.mean())
 
-		#torch.save(self.actor.state_dict(), '%s/%s_actor.pth' % (directory, filename))
-
+		
 
 
